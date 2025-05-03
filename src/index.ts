363a9f9aa96a5e777511bc9ce31b63aae02f54fe
MRxTHUNDER <sahilchahal221@gmail.com>
2025-05-04 04:49:40 +0530

    cors error fixed on local

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ mongoConnect();
 
 const corsOptions={
     origin: process.env.NODE_ENV === 'dev'?'http://localhost:5173':'https://blockstore.in',
-    method:['GET','POST','PUT','DELETE'],
+    methods:['GET','POST','PUT','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-VERIFY', 'X-MERCHANT-ID'],
     credentials:true
 }
